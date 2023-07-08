8bcff38c85c1c39692e475f5ba1b76da3ef5dafd
Pavel <web2cap@gmail.com>
2023-07-08 10:29:18 +0200

    react leaflet bootstrap implement

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Map from './Map';
+
+function App() {
+    return (
+        <div>
+            <h1>Welcome to My App</h1>
+            <Map />
+        </div>
+    );
+}
+
+export default App;
+
+
