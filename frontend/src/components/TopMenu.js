ddc7839cbcc39274439515e744619d0452bca321
Pavel <web2cap@gmail.com>
2023-07-14 13:53:00 +0200

    registration form beta

--- frontend/src/components/TopMenu.js
+++ frontend/src/components/TopMenu.js
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 import LoginForm from './LoginForm';
+import RegistrationForm from './RegistrationForm';
 
 function TopMenu() {
     const [showLogin, setShowLogin] = useState(false);
+    const [showRegistration, setShowRegistration] = useState(false);
     const [accessToken, setAccessToken] = useState('');
     const [user, setUser] = useState(null);
-    const [error, setError] = useState('');
 
     useEffect(() => {
+        // Check if access token is stored in localStorage
         const storedAccessToken = localStorage.getItem('accessToken');
         if (storedAccessToken) {
             setAccessToken(storedAccessToken);
@@ -23,43 +25,89 @@ function TopMenu() {
         setShowLogin(false);
     };
 
-    const handleLoginSubmit = async (email, password) => {
+    const handleRegistrationClick = () => {
+        setShowRegistration(true);
+    };
+
+    const handleCloseRegistration = () => {
+        setShowRegistration(false);
+    };
+
+    const handleLoginSubmit = (email, password) => {
         const requestBody = {
             email,
             password,
         };
 
-        try {
-            const response = await fetch('http://localhost:8000/api/v1/auth/jwt/create/', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(requestBody),
+        // Make the API call to `/api/v1/auth/jwt/create` with the requestBody
+        // Replace `YOUR_API_ENDPOINT` with the actual endpoint URL
+        fetch('http://localhost:8000/api/v1/auth/jwt/create/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(requestBody),
+        })
+            .then((response) => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    throw new Error('Login failed'); // Throw an error if the login request fails
+                }
+            })
+            .then((data) => {
+                // Handle the successful response from the API
+                console.log(data); // Display or process the response data as needed
+                setShowLogin(false); // Close the login modal
+                setAccessToken(data.access); // Store the access token in state
+                localStorage.setItem('accessToken', data.access); // Store the access token in localStorage
+                setEmail(''); // Reset the email field
+                setPassword(''); // Reset the password field
+            })
+            .catch((error) => {
+                // Handle the error from the login request
+                console.error(error);
             });
+    };
 
-            if (response.ok) {
-                const data = await response.json();
-                setAccessToken(data.access);
-                localStorage.setItem('accessToken', data.access);
-                setShowLogin(false);
-                setError('');
-            } else {
-                throw new Error('Login failed');
-            }
-        } catch (error) {
-            console.error(error);
-            setError('Login failed. Please check your credentials.');
-        }
+    const handleRegistrationSubmit = (userData) => {
+        // Make the API call to `/api/v1/auth/users/` with the userData
+        // Replace `YOUR_API_ENDPOINT` with the actual endpoint URL
+        fetch('http://localhost:8000/api/v1/auth/users/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(userData),
+        })
+            .then((response) => {
+                if (response.status === 201) {
+                    // Registration success, now perform login
+                    handleLoginSubmit(userData.email, userData.password);
+                } else {
+                    return response.json();
+                }
+            })
+            .then((data) => {
+                // Handle the error response from the registration request
+                console.error(data);
+            })
+            .catch((error) => {
+                // Handle the error from the registration request
+                console.error(error);
+            });
     };
 
     const handleLogout = () => {
-        setAccessToken('');
-        localStorage.removeItem('accessToken');
+        setAccessToken(''); // Clear the access token from state
+        localStorage.removeItem('accessToken'); // Remove the access token from localStorage
     };
 
     useEffect(() => {
+        // Fetch user details from `/api/v1/auth/users/me` if access token is available
         if (accessToken) {
+            // Make the API call to `/api/v1/auth/users/me`
+            // Replace `YOUR_API_ENDPOINT` with the actual endpoint URL
             fetch('http://localhost:8000/api/v1/auth/users/me', {
                 method: 'GET',
                 headers: {
@@ -70,16 +118,18 @@ function TopMenu() {
                     if (response.ok) {
                         return response.json();
                     } else {
-                        throw new Error('User details fetch failed');
+                        throw new Error('User details fetch failed'); // Throw an error if the user details fetch request fails
                     }
                 })
                 .then((data) => {
-                    console.log(data);
-                    setUser(data);
+                    // Handle the successful response from the API
+                    console.log(data); // Display or process the response data as needed
+                    setUser(data); // Set the user details in state
                 })
                 .catch((error) => {
+                    // Handle the error from the user details fetch request
                     console.error(error);
-                    setUser(null);
+                    setUser(null); // Clear the user details in state
                 });
         }
     }, [accessToken]);
@@ -105,14 +155,19 @@ function TopMenu() {
                         ) : (
                             <>
                                 <Nav.Link onClick={handleLoginClick}>Login</Nav.Link>
-                                <Nav.Link href="#">Join</Nav.Link>
+                                <Nav.Link onClick={handleRegistrationClick}>Join</Nav.Link>
                             </>
                         )}
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
 
-            <LoginForm show={showLogin} handleClose={handleCloseLogin} handleLoginSubmit={handleLoginSubmit} error={error} />
+            <LoginForm show={showLogin} handleClose={handleCloseLogin} handleLoginSubmit={handleLoginSubmit} />
+            <RegistrationForm
+                show={showRegistration}
+                handleClose={handleCloseRegistration}
+                handleRegistrationSubmit={handleRegistrationSubmit}
+            />
         </>
     );
 }
