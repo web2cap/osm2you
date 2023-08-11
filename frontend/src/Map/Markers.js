eea951bb1ad64caeb690d3d61aa74113cf4dbce6
Pavel <web2cap@gmail.com>
2023-08-11 16:54:20 +0200

    lazy loading

--- frontend/src/Map/Markers.js
+++ frontend/src/Map/Markers.js
@@ -1,18 +1,27 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { useStoreState } from 'easy-peasy';
 
-import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
+import { Marker, Popup, useMap } from 'react-leaflet';
 
 const Markers = () => {
     const MARKERS_URL = '/api/v1/markers/';
+    const DEBOUNCE_DELAY = 300
     const backend = useStoreState((state) => state.backend);
 
     const [markers, setMarkers] = useState([]);
+    const [bbox, setBbox] = useState(null)
 
+    const isFirstCallRef = useRef(true);
+    const debounceTimerRef = useRef(null);
+
+    const map = useMap();
+
+    // fetch markers
     useEffect(() => {
         async function fetchData() {
+            const url = bbox ? `${MARKERS_URL}?in_bbox=${bbox}` : MARKERS_URL
             try {
-                const response = await backend.get(MARKERS_URL);
+                const response = await backend.get(url);
                 if (response.status !== 200) {
                     throw TypeError("Failed");
                 }
@@ -22,7 +31,46 @@ const Markers = () => {
             }
         }
         fetchData();
-    }, []);
+    }, [bbox, backend]);
+
+
+    // set bbox
+    const handleMapChange = () => {
+        if (isFirstCallRef.current) {
+            isFirstCallRef.current = false;
+            callLogic();
+            debounceSubsequentCalls();
+        } else {
+            clearTimeout(debounceTimerRef.current);
+            debounceSubsequentCalls();
+        }
+    };
+
+    const debounceSubsequentCalls = () => {
+        debounceTimerRef.current = setTimeout(() => {
+            callLogic();
+            clearTimeout(debounceTimerRef.current);
+        }, DEBOUNCE_DELAY);
+    };
+
+    const callLogic = () => {
+        const bounds = map.getBounds();
+        const bbox = bounds.toBBoxString();
+        setBbox(bbox)
+        console.log('Map bbox', bbox);
+    };
+
+    useEffect(() => {
+        map.on('move', handleMapChange);
+        map.on('zoom', handleMapChange);
+
+        return () => {
+            map.off('move', handleMapChange);
+            map.off('zoom', handleMapChange);
+            clearTimeout(debounceTimerRef.current);
+        };
+    }, [map]);
+
 
     return (
         <>
