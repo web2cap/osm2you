b061fc1c72027e5a149eea5f388583fe59090ba6
Pavel <web2cap@gmail.com>
2023-08-16 17:42:55 +0200

    marker instance delete

--- frontend/src/Map/MarkerInstance.js
+++ frontend/src/Map/MarkerInstance.js
@@ -1,14 +1,16 @@
 import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
+import { Link, useParams, useNavigate } from "react-router-dom"
 import { useStoreState, useStoreActions } from 'easy-peasy';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 import Missing from "../Template/Missing"
+import './MarkerInstance.css'
 
 const MarkerInstance = () => {
     const { id } = useParams()
 
     const [errMsg, setErrMsg] = useState('')
+    const [errMissing, setErrMissing] = useState(false)
 
     const MARKERS_URL = useStoreState((state) => state.MARKERS_URL)
     const backend = useStoreState((state) => state.backend);
@@ -17,6 +19,9 @@ const MarkerInstance = () => {
 
     const [marker, setMarker] = useState(null)
 
+    const navigate = useNavigate()
+
+    // fetch marker
     useEffect(() => {
         setShowAddingMarker(false)
         async function fetchMarker(id) {
@@ -29,30 +34,68 @@ const MarkerInstance = () => {
                 setMarker(response.data);
             } catch (err) {
                 setErrMsg(`Error fetching marker: ${err}`)
+                setErrMissing(true)
                 console.error(`Error fetching marker: ${err}`);
             }
         }
         fetchMarker(id)
     }, [])
 
+    //delete
+    const handleDelete = async () => {
+        setErrMsg('');
+        try {
+            const response = await backend.delete(`${MARKERS_URL}${id}`,
+                { headers: { 'Content-Type': 'application/json' } }
+            );
+            if (response.status !== 204) {
+                throw TypeError("Deletion filed")
+            }
+            navigate('/')
+        } catch (err) {
+            console.log(err)
+            setErrMsg(err?.response?.data?.detail
+                ? err.response.data.detail
+                : "Deletion filed"
+            )
+        }
+    }
+
     return (
         <main>
-            {errMsg ? <>
-                <div className='errmsg' aria-live="assertive">{errMsg}</div>
-                <Missing />
-            </> :
-                <div className="map-container">
-                    {marker && <MapContainer center={[marker.geometry.coordinates[1], marker.geometry.coordinates[0]]} zoom={12} className='MapContainer'>
-                        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            {errMsg && <div className='errmsg' aria-live="assertive">{errMsg}</div>}
+            {errMissing
+                ? <Missing />
+                : marker && <div>
+                    <div className="map-container">
+                        <MapContainer center={[marker.geometry.coordinates[1], marker.geometry.coordinates[0]]} zoom={12} className='MapInstanceContainer'>
+                            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
 
-                        <Marker position={[marker.geometry.coordinates[1], marker.geometry.coordinates[0]]}>
-                            <Popup>
-                                {marker.properties.name}
-                            </Popup>
-                        </Marker>
+                            <Marker position={[marker.geometry.coordinates[1], marker.geometry.coordinates[0]]}>
+                                <Popup>
+                                    {marker.properties.name}
+                                </Popup>
+                            </Marker>
 
-                    </MapContainer>
-                    }
+                        </MapContainer>
+
+                    </div>
+                    <div className="MarkerInfo">
+                        <h1 className="marker-name">{marker.properties.name}</h1>
+                        <div className="button-group">
+                            <button>Edit</button>
+                            <button onClick={handleDelete} className="delete">Delete</button>
+                            <button className="add">Add story</button>
+                        </div>
+                        <p className="description"></p>
+                        <ul className="related-markers">
+                            {/* marker.relatedMarkers.map(relatedMarker => (
+                                <li key={relatedMarker.id}>
+                                    <Link to={`/marker/${relatedMarker.id}`} className="related-marker-link">{relatedMarker.name}</Link>
+                                </li>
+                            )) */}
+                        </ul>
+                    </div>
                 </div>
             }
         </main >
