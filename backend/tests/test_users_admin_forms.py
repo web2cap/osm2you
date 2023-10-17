ebf970ec3edc6af4abf6f6e9abd8df86f95b4e67
Pavel <web2cap@gmail.com>
2023-10-17 21:48:47 +0200

    fix test in classes

--- backend/tests/test_users_admin_forms.py
+++ backend/tests/test_users_admin_forms.py
@@ -1,35 +1,33 @@
 import pytest
 
-from users.admin import CustomUserCreationForm, CustomUserChangeForm
+from users.admin import CustomUserChangeForm, CustomUserCreationForm
 
 
-@pytest.mark.django_db
-def test_custom_user_creation_form_valid(sample_admin_form_user_data):
-    """If the CustomUserCreationForm is valid with valid form data."""
+class TestUsersAdminForms:
+    @pytest.mark.django_db
+    def test_custom_user_creation_form_valid(self, sample_admin_form_user_data):
+        """If the CustomUserCreationForm is valid with valid form data."""
 
-    form = CustomUserCreationForm(sample_admin_form_user_data)
-    assert form.is_valid()
+        form = CustomUserCreationForm(sample_admin_form_user_data)
+        assert form.is_valid()
 
+    @pytest.mark.django_db
+    def test_custom_user_creation_form_invalid(self):
+        """If the CustomUserCreationForm is invalid with empty form data."""
 
-@pytest.mark.django_db
-def test_custom_user_creation_form_invalid():
-    """If the CustomUserCreationForm is invalid with empty form data."""
+        form = CustomUserCreationForm(dict())
+        assert not form.is_valid()
 
-    form = CustomUserCreationForm(dict())
-    assert not form.is_valid()
+    @pytest.mark.django_db
+    def test_custom_user_change_form_valid(self, admin_form_user_data_updated):
+        """If the CustomUserChangeForm is valid with updatet form data."""
 
+        form = CustomUserChangeForm(admin_form_user_data_updated)
+        assert form.is_valid()
 
-@pytest.mark.django_db
-def test_custom_user_change_form_valid(admin_form_user_data_updated):
-    """If the CustomUserChangeForm is valid with updatet form data."""
+    @pytest.mark.django_db
+    def test_custom_user_change_form_invalid(self):
+        """If the CustomUserChangeForm is invalid with empty form data."""
 
-    form = CustomUserChangeForm(admin_form_user_data_updated)
-    assert form.is_valid()
-
-
-@pytest.mark.django_db
-def test_custom_user_change_form_invalid():
-    """If the CustomUserChangeForm is invalid with empty form data."""
-
-    form = CustomUserChangeForm(dict())
-    assert not form.is_valid()
+        form = CustomUserChangeForm(dict())
+        assert not form.is_valid()
