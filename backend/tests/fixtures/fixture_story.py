cf89e4d3731132c73169b44989391b1626d280ab
Pavel <web2cap@gmail.com>
2023-10-26 23:04:08 +0200

    test stories models

--- backend/tests/fixtures/fixture_story.py
+++ backend/tests/fixtures/fixture_story.py
@@ -13,6 +13,11 @@ def simple_story_data(user_owner_instance):
 
 
 @pytest.fixture
+def story_with_marker_data(simple_story_data, simple_marker):
+    return simple_story_data | {"marker": simple_marker}
+
+
+@pytest.fixture
 def story_viewset():
     return StoryViewSet()
 
---
+++ backend/tests/stories/test_models.py
@@ -0,0 +1,38 @@
+import pytest
+from django.db.utils import IntegrityError
+
+from stories.models import Story
+
+
+class TestStoriesModels:
+    @pytest.mark.django_db
+    def test_create_story(self, story_with_marker_data):
+        """Test for creating story."""
+
+        story = Story.objects.create(**story_with_marker_data)
+        assert (
+            story.text == story_with_marker_data["text"]
+        ), "Created story has wrong text"
+        assert (
+            story.author == story_with_marker_data["author"]
+        ), "Created story has wrong author"
+        assert (
+            story.marker == story_with_marker_data["marker"]
+        ), "Created story has wrong marker"
+
+    @pytest.mark.django_db
+    def test_create_marker_no_text(self, story_with_marker_data):
+        """Test creating a story with no text."""
+
+        story_with_marker_data.pop("text")
+        with pytest.raises(IntegrityError):
+            story = Story.objects.create(**story_with_marker_data)
+            story.save()
+
+    @pytest.mark.django_db
+    def test_create_marker_no_marker(self, simple_story_data):
+        """Test creating a story with no marker."""
+
+        with pytest.raises(IntegrityError):
+            story = Story.objects.create(**simple_story_data)
+            story.save()
