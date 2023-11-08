7be123d05565984668c7bd3ff5930f2700732e12
Pavel <web2cap@gmail.com>
2023-11-08 22:17:31 +0100

    fix drf serializer abstract test

--- tests/unit/api/drf_unit_pytest.py
+++ tests/unit/api/drf_unit_pytest.py
@@ -5,11 +5,13 @@ import pytest
 
 @pytest.fixture
 def simple_instance():
+    # Implement this fixture to provide a sample model instance for testing
     pass
 
 
 @pytest.fixture
 def simple_json():
+    # Implement this fixture to provide valid JSON data for serializer testing
     pass
 
 
@@ -17,24 +19,28 @@ class AbstractTestSerializer(ABC):
     @property
     @abstractmethod
     def serializer_class(self):
-        """Subclasses must define serializer."""
+        """Subclasses must define the serializer class."""
         pass
 
     @property
     @abstractmethod
     def fields_must_present(self):
-        """Subclasses must define list of fields, which must present in response."""
+        """Subclasses must define a list of fields that must be present in the response."""
         pass
 
     @abstractmethod
-    def fields_must_equal(self, simple_instance):
-        """Subclasses must return dict of fields, with expected values."""
+    def expected_data(self, simple_instance):
+        """Subclasses must return a dictionary of fields with expected values.
+        return example = {
+            "text": simple_instance.text,
+            "author": simple_instance.author.id,
+        }."""
         pass
 
     @property
     def read_only_serializer(self):
-        """Subclasses must define read_only_serializer property to False
-        if serializer only for safe methods."""
+        """Subclasses must define the read_only_serializer property to True
+        if serializer is only for safe methods."""
         return False
 
     @pytest.mark.django_db
@@ -46,36 +52,36 @@ class AbstractTestSerializer(ABC):
         serializer = self.serializer_class(data=simple_json)
         assert (
             serializer.is_valid()
-        ), f"Error serialize with valid data {serializer.errors}"
+        ), f"Error serializing with valid data: {serializer.errors}"
 
     @pytest.mark.django_db
     def test_serializer_data_present(self, simple_instance):
-        """Check that needet data includet."""
+        """Check that required data is included in the serialized output."""
 
         serializer = self.serializer_class(instance=simple_instance)
 
         for must_present in self.fields_must_present:
             assert (
                 must_present in serializer.data
-            ), f"Field {must_present} should present in valid data."
+            ), f"Field {must_present} should be present in valid data."
 
     @pytest.mark.django_db
     def test_serializer_data_equal(self, simple_instance):
         serializer = self.serializer_class(instance=simple_instance)
-        fields_must_equal = self.fields_must_equal(simple_instance)
+        expected_data = self.expected_data(simple_instance)
 
-        for field in fields_must_equal:
+        for field, expected_value in expected_data.items():
             assert (
-                serializer.data[field] == fields_must_equal[field]
-            ), f"{field} in serialized data not eqal user model data"
+                serializer.data[field] == expected_value
+            ), f"{field} in serialized data does not match the expected value."
 
     @pytest.mark.django_db
-    def test_serializer_excludet_data(self, simple_instance):
-        """Check that no extra fields includet."""
+    def test_serializer_excluded_data(self, simple_instance):
+        """Check that no extra fields are included in the serialized output."""
 
         serializer = self.serializer_class(instance=simple_instance)
         for field in simple_instance._meta.fields:
             assert (
                 field.name in self.fields_must_present
                 or field.name not in serializer.data
-            ), f"Field {field.name} should not present in valid data."
+            ), f"Field {field.name} should not be present in valid data."
