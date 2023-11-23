6c030852084ae02a226376b2d630b4db520daec0
Pavel <web2cap@gmail.com>
2023-11-23 17:49:41 +0100

    stories list

--- frontend/src/Map/MarkerUser.js
+++ frontend/src/Map/MarkerUser.js
@@ -1,17 +1,18 @@
-import { useParams, useNavigate } from "react-router-dom"
+import { useParams, useNavigate, Link } from "react-router-dom"
 import { MapContainer, TileLayer, } from 'react-leaflet';
 import { useStoreState } from 'easy-peasy'
 
 import LocationMarker from './LocationMarker';
 import Markers from './Markers';
 import './MarkerUser.css';
+import Stories from "../Story/Stories";
 
 
 
 const MarkerUser = () => {
 
     const mapCenter = useStoreState((state) => state.mapCenter)
-    const navigate = useNavigate()
+    const markers = useStoreState((state) => state.markers)
     const { username } = useParams()
 
     
@@ -24,7 +25,20 @@ const MarkerUser = () => {
                 <LocationMarker />
             </MapContainer>
         </div>
-                    
+        <div className="UserInfo">
+            <h1 className="marker-name">{username}</h1>
+            <>
+            {markers.map(marker => (
+              <div>
+                <Link to={`/markers/${marker.id}`} className="story-marker">
+                    <h3>{marker.properties.name}</h3>
+                </Link>
+                <Stories stories_list={marker.properties.stories}  />
+              </div>      
+            ))}
+            </>
+            
+        </div>     
            
         </main >
     )
