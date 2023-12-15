d45cade57e4af47b7ded86c95752ca333b5858a5
Pavel <web2cap@gmail.com>
2023-12-15 11:34:26 +0100

    tag values model

--- backend/tags/models.py
+++ backend/tags/models.py
@@ -2,6 +2,7 @@ from core.models import CreatedModel
 from django.db import models
 from django.db.models import CharField, Q
 from django.db.models.functions import Length
+from markers.models import Marker
 
 CharField.register_lookup(Length, "length")
 
@@ -27,3 +28,41 @@ class Tag(CreatedModel):
 
     def __str__(self):
         return self.name
+
+
+class TagValue(CreatedModel):
+    """Tags values for marker"""
+
+    tag = models.ForeignKey(
+        Tag,
+        on_delete=models.CASCADE,
+        blank=False,
+        null=False,
+        related_name="tag_value",
+        verbose_name="Tag",
+        help_text="Specify value for tag",
+    )
+    marker = models.ForeignKey(
+        Marker,
+        on_delete=models.CASCADE,
+        blank=False,
+        null=False,
+        related_name="tag_value",
+        verbose_name="Marker",
+        help_text="Choice marker",
+    )
+    value = models.CharField(max_length=255, null=True, blank=False, default=None)
+
+    class Meta:
+        ordering = ("-tag",)
+        verbose_name = "Tag value"
+        verbose_name_plural = "Tags values"
+        constraints = [
+            models.CheckConstraint(
+                check=Q(value__length__gte=1),
+                name="value_min_length",
+            )
+        ]
+
+    def __str__(self):
+        return self.tag
