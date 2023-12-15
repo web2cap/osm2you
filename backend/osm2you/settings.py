ce79f4f752af11428ac8ab4c57f120419db1e46f
Pavel <web2cap@gmail.com>
2023-12-15 10:49:17 +0100

    tag model

--- backend/osm2you/settings.py
+++ backend/osm2you/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     "rest_framework.authtoken",
     "users",
     "markers",
+    "tags",
     "stories",
     "api",
     "drf_yasg",
--- backend/tags/models.py
+++ backend/tags/models.py
@@ -1,3 +1,14 @@
+from core.models import CreatedModel
 from django.db import models
 
-# Create your models here.
+
+class Tag(CreatedModel):
+    """Tags clasifficator with human-friendly name"""
+
+    name = models.CharField(max_length=255, null=False, blank=False)
+    display_name = models.CharField(
+        max_length=255, null=True, blank=False, default=None
+    )
+
+    def __str__(self):
+        return self.name
