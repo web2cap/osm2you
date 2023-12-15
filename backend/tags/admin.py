911b4c5da922308a9a9e119d6b6e7c13e60e31d0
Pavel <web2cap@gmail.com>
2023-12-15 10:55:50 +0100

    tag admin

--- backend/tags/admin.py
+++ backend/tags/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Tag
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("pk", "name", "display_name", "created")
+    search_fields = ("name", "display_name")
+
+
+admin.site.register(Tag, TagAdmin)
