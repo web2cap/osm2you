32109498d9b8c7ba577aee5c69ebd02921f96895
Pavel <web2cap@gmail.com>
2023-12-15 10:41:57 +0100

    overpass query

--- osmmainer/app/mainer.py
+++ osmmainer/app/mainer.py
@@ -1,6 +1,6 @@
+from app.db import Marker, session
+from app.overpass import overpass_camp_site
 from sqlalchemy.exc import SQLAlchemyError
-
-from .db import Marker, session
 
 
 def mainer():
@@ -13,3 +13,41 @@ def mainer():
         print(f"Error: {e}")
     finally:
         session.close()
+
+    south = -90
+    west = -180
+    north = 90
+    east = 180
+
+    from bs4 import BeautifulSoup
+
+    # Assuming 'response.text' contains the XML data
+    xml_data = overpass_camp_site(south, west, north, east)
+
+    # Parse the XML data with BeautifulSoup
+    soup = BeautifulSoup(xml_data, "xml")
+
+    tags = {}
+
+    # Now you can navigate and extract information from the XML structure
+    for node in soup.find_all("node"):
+        node_id = node.get("id")
+        lat = node.get("lat")
+        lon = node.get("lon")
+        name_tag = node.find("tag", {"k": "name"})
+        name = name_tag.get("v") if name_tag else None
+        tourism_tag = node.find("tag", {"k": "tourism"})
+        tourism = tourism_tag.get("v") if tourism_tag else None
+        print(
+            f"Node ID: {node_id}, Lat: {lat}, Lon: {lon}, Name: {name}, Tourism: {tourism}"
+        )
+        for tag in node.find_all("tag"):
+            tag_k = tag.get("k")
+            if tag_k:
+                tags[tag_k] = tags.get(tag_k, 0) + 1
+                print(f"\t{tag_k}")
+
+    # tag statistics
+
+    for tag in tags:
+        print(f"{tags[tag]}:\t{tag}")
