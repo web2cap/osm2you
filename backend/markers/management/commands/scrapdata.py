bf99f7923e1d5ed6b8e4bfaa8a7faf18c99f6b1c
Pavel <web2cap@gmail.com>
2024-01-09 12:24:32 +0100

    clear debug, fix docstring

--- backend/markers/management/commands/scrapdata.py
+++ backend/markers/management/commands/scrapdata.py
@@ -10,20 +10,16 @@ from markers.management.utils.scrap import scrap_nodes
 
 logger = logging.getLogger(__name__)
 
-SCRAPDATA_SUCCESS_MESSAGE = getattr(settings, "SCRAPDATA_SUCCESS_MESSAGE", {})
-SCRAPDATA_FAILURE_MESSAGE = getattr(settings, "SCRAPDATA_FAILURE_MESSAGE", {})
-
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        """Scrap markers from openstreetmap."""
+        """Scrap markers from openstreetmap.org by using overpass-turbo.eu.
+        Fills markers and tags with them values into database.
+        Logs the main steps."""
 
         try:
             logger.warning("Start loading overpass xml data")
             xml_data = overpass_camp_site(south=0, west=0, north=90, east=180)
-            # debug with fake overpass data
-            # f = open("overpass.xml", "r")
-            # xml_data = f.read()
             xml_data_size = sys.getsizeof(xml_data) / 1024
             logger.warning(f"XML loadded, size {xml_data_size:.2f} KB")
             logger.warning("Start scrapping xml data")
