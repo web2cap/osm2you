6e08e59db0ab137dd449ccd88dc0a3881082f7dc
Pavel <web2cap@gmail.com>
2024-01-09 12:17:59 +0100

    try-except ext response

--- backend/markers/management/utils/overpass.py
+++ backend/markers/management/utils/overpass.py
@@ -30,10 +30,12 @@ def overpass_camp_site(south=-90, west=-180, north=90, east=180):
             south=south, west=west, north=north, east=east
         )
     }
-    response = requests.get(OVERPASS["url"], params=params)
-
-    if response.status_code == 200:
-        return response.text
+    try:
+        response = requests.get(OVERPASS["url"], params=params)
+        if response.status_code == 200:
+            return response.text
+    except Exception as e:
+        logger.error(f"overpass_camp_site {e}")
     else:
         logger.error(f"overpass_camp_site response {response.status_code}")
         return None
