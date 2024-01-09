a031f6ec3d1cc7783c3d2c4aa59cd3c024be6bdc
Pavel <web2cap@gmail.com>
2024-01-09 11:46:43 +0100

    add logging

--- backend/markers/management/utils/filldb.py
+++ backend/markers/management/utils/filldb.py
@@ -1,6 +1,10 @@
+import logging
+
 from django.contrib.gis.geos import Point
 from markers.models import Marker
 from tags.models import Tag, TagValue
+
+logger = logging.getLogger(__name__)
 
 
 def update_nodes(nodes):
@@ -34,46 +38,63 @@ def update_nodes(nodes):
         - The function updates or creates markers, tags, and tag values in an atomic transaction for data consistency.
     """
 
-    try:
-        for node in nodes:
+    for node in nodes:
+        stat = {
+            "markers_upd": 0,
+            "markers_add": 0,
+            "tags_use": 0,
+            "tags_add": 0,
+            "tv_upd": 0,
+            "tv_add": 0,
+        }
+
+        try:
             # Marker
             location = Point(float(node["lon"]), float(node["lat"]))
             marker_queryset = Marker.objects.filter(location__exact=location)
             if marker_queryset.exists():  # get marker
                 marker = marker_queryset.first()
-                if node["name"] and not marker.name:  # update marker name
+                if not marker.name:  # update marker name
                     marker.name = (
                         node["name"]["v"]
                         if isinstance(node["name"], dict)
                         else node["name"]
                     )
                     marker.save()
+                    stat["markers_upd"] += 1
+
             else:  # create marker
                 marker = Marker.objects.create(name=node.get("name"), location=location)
+                stat["markers_add"] += 1
             # Tag
             for tag_name, tag_value in node["tags"].items():
                 tag_queryset = Tag.objects.filter(name=tag_name)
                 if tag_queryset.exists():  # get tag
                     tag = tag_queryset.first()
+                    stat["tags_use"] += 1
                 else:  # create tag
                     tag = Tag.objects.create(name=tag_name)
-                    # TagValue
-                    marker_tag_value_queryset = TagValue.objects.filter(
-                        marker=marker, tag=tag
+                    stat["tags_add"] += 1
+
+                # TagValue
+                marker_tag_value_queryset = TagValue.objects.filter(
+                    marker=marker, tag=tag
+                )
+                if marker_tag_value_queryset.exists():  # get value
+                    marker_tag_value = marker_tag_value_queryset.first()
+                    # update value
+                    if tag_value and marker_tag_value.value != tag_value:
+                        marker_tag_value.value = tag_value
+                        marker_tag_value.save()
+                        stat["tv_upd"] += 1
+                else:  # create marker tag value
+                    if not tag_value:
+                        tag_value = None
+                    marker_tag_value = TagValue.objects.create(
+                        marker=marker, tag=tag, value=tag_value
                     )
-                    if marker_tag_value_queryset.exists():  # get value
-                        marker_tag_value = marker_tag_value_queryset.first()
-                        # update value
-                        if tag_value and marker_tag_value.value != tag_value:
-                            marker_tag_value.value = tag_value
-                            marker_tag_value.save()
-                    else:  # create marker tag value
-                        if not tag_value:
-                            tag_value = None
-                        marker_tag_value = TagValue.objects.create(
-                            marker=marker, tag=tag, value=tag_value
-                        )
-        return True
-    except Exception as e:
-        print(f"update_nodes error: {e}")
-        return False
+                    stat["tv_add"] += 1
+        except Exception as e:
+            logger.exception(f"update_nodes {e}\n On node: {node}")
+
+    return stat
