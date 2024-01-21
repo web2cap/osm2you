d9b4079ec5f2c25ac057dc8b0ef81716b46ff908
Pavel <web2cap@gmail.com>
2024-01-21 14:06:48 +0100

    flower

--- infra/rebuild_app.sh
+++ infra/rebuild_app.sh
@@ -2,4 +2,5 @@ docker-compose down
 docker image rm infra_osm_celery_worker
 docker image rm infra_osm_celery_beat
 docker image rm infra_osm_back
+docker image rm infra_osm_flower
 docker-compose up            
