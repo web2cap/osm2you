d41edaaff86285f4b0babae2068d2276ae7f4558
Pavel <web2cap@gmail.com>
2024-02-03 20:04:51 +0100

    overpass by qyery

--- backend/markers/management/utils/overpass.py
+++ backend/markers/management/utils/overpass.py
@@ -4,7 +4,30 @@ import requests
 from django.conf import settings
 
 OVERPASS = getattr(settings, "OVERPASS", {})
+MARKERS_KIND_MAIN = getattr(settings, "MARKERS_KIND_MAIN", {})
+
 logger = logging.getLogger(__name__)
+
+
+def overpass_by_query(overpass_query):
+    """
+    Query the Overpass API with prepeared query.
+
+    Args:
+        overpass_query (str): overpass API query.
+    Returns:
+        str or None: Overpass API response text if successful, None otherwise.
+    """
+
+    try:
+        response = requests.get(OVERPASS["url"], params={"data": overpass_query})
+        if response.status_code == 200:
+            return response.text
+    except Exception as e:
+        logger.error(f"overpass {e}")
+    else:
+        logger.error(f"overpass response {response.status_code}")
+        return None
 
 
 def overpass_camp_site(south=-90, west=-180, north=90, east=180):
@@ -24,18 +47,11 @@ def overpass_camp_site(south=-90, west=-180, north=90, east=180):
         - The bounding box is defined by the parameters (south, west, north, east).
         - The Overpass API URL and query template are fetched from the Django settings.
     """
-
-    params = {
-        "data": OVERPASS["camp_site"].format(
-            south=south, west=west, north=north, east=east
-        )
-    }
-    try:
-        response = requests.get(OVERPASS["url"], params=params)
-        if response.status_code == 200:
-            return response.text
-    except Exception as e:
-        logger.error(f"overpass_camp_site {e}")
-    else:
-        logger.error(f"overpass_camp_site response {response.status_code}")
-        return None
+    overpass_query = OVERPASS["main"].format(
+        tag=f"{MARKERS_KIND_MAIN['tag']}={MARKERS_KIND_MAIN['tag_value']}",
+        south=south,
+        west=west,
+        north=north,
+        east=east,
+    )
+    return overpass_by_query(overpass_query)
