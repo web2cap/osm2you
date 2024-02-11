da6e8e1bd77dc5b8410656efee200db1872680c8
Pavel <web2cap@gmail.com>
2024-02-11 17:44:30 +0100

    add kind to response

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -188,7 +188,11 @@ class KindSerializer(serializers.ModelSerializer):
     icon = serializers.CharField(source="kind_group.icon", read_only=True)
     tag = serializers.CharField(source="tag.name", read_only=True)
     tag_display_name = serializers.CharField(source="tag.display_name", read_only=True)
+    kind = serializers.SerializerMethodField()
+
+    def get_kind(self, obj):
+        return f"{obj.tag.name}={obj.value}"
 
     class Meta:
         model = Kind
-        fields = ("tag", "tag_display_name", "value", "color", "icon")
+        fields = ("tag", "tag_display_name", "value", "color", "icon", "kind")
