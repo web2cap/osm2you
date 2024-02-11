fb73fdf46ab3746e57b725c76e4d8b2f333df8ea
Pavel <web2cap@gmail.com>
2024-02-11 21:33:37 +0100

    related marker icon

--- frontend/src/Map/RelatedMarker.js
+++ frontend/src/Map/RelatedMarker.js
@@ -1,18 +1,38 @@
-
 import { Link } from 'react-router-dom';
 import { Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
+import "leaflet-extra-markers";
+import 'leaflet-extra-markers/dist/css/leaflet.extra-markers.min.css'; 
+import "@fortawesome/fontawesome-free/css/all.css";
 
-const RelatedMarker = ({marker, kinds}) => {
+const RelatedMarker = ({ marker, kinds }) => {
 
+  const findKind = (kindValue) => {
+    return kinds.find((kind) => kind.kind === kindValue);
+  };
 
+  const createIcon = (kind) => {
+
+    return L.ExtraMarkers.icon({
+      icon: kind ? kind.icon : 'fa-leaf',
+      iconColor: 'white',
+      markerColor: kind ? kind.color : 'yellow',
+      shape: 'circle',
+      prefix: 'fa',
+    });
+  };
+
+  const kind = findKind(marker.properties.kind);
+  const icon = createIcon(kind)
 
   return (
     <Marker
         key={marker.id}
-         position={[
+        position={[
            marker.geometry.coordinates[1],
            marker.geometry.coordinates[0],
-         ]}
+        ]}
+        icon={icon} 
     >
         <Popup>
           <Link to={`/markers/${marker.id}`}>
@@ -20,7 +40,7 @@ const RelatedMarker = ({marker, kinds}) => {
           </Link>
         </Popup>
     </Marker>
-  )
-}
+  );
+};
 
-export default RelatedMarker
+export default RelatedMarker;
