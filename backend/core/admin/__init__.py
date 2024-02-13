3a357f1efc5b75c9b979e31c9275783e1031175b
Pavel <web2cap@gmail.com>
2024-02-13 15:01:14 +0100

    move stories admin

--- backend/core/admin/__init__.py
+++ backend/core/admin/__init__.py
@@ -1,7 +1,9 @@
+from .stories import StoryAdmin
 from .tags import KindAdmin, KindGroupAdmin, TagAdmin
 from .users import CustomUserAdmin
 
 __all__ = [
+    StoryAdmin,
     KindAdmin,
     KindGroupAdmin,
     TagAdmin,
