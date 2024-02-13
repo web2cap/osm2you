33d4b42ce6b849dc55ebdff18b9276e38dcf7058
Pavel <web2cap@gmail.com>
2024-02-13 15:04:29 +0100

    move markers admin

--- backend/core/admin/__init__.py
+++ backend/core/admin/__init__.py
@@ -1,8 +1,10 @@
+from .markers import MarkerAdmin
 from .stories import StoryAdmin
 from .tags import KindAdmin, KindGroupAdmin, TagAdmin
 from .users import CustomUserAdmin
 
 __all__ = [
+    MarkerAdmin,
     StoryAdmin,
     KindAdmin,
     KindGroupAdmin,
