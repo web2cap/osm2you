f5c4b6d18d4d37ff311ba35431a53661f38d5873
Pavel <web2cap@gmail.com>
2024-02-13 14:48:09 +0100

    move markers models

--- backend/core/models/__init__.py
+++ backend/core/models/__init__.py
@@ -1,7 +1,12 @@
 from .create import CreatedModel
+from .markers import Marker, MarkerCluster, MarkerClusterMixin, UpdatedMarkerCluster
 from .users import User
 
 __all__ = [
     CreatedModel,
+    Marker,
+    MarkerCluster,
+    MarkerClusterMixin,
+    UpdatedMarkerCluster,
     User,
 ]
