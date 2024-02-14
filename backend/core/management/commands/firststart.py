e85b46cccc8e1a295cc70edc62525e1aae090e28
Pavel <web2cap@gmail.com>
2024-02-14 17:57:21 +0100

    separate hendle by models entities

--- backend/core/management/commands/firststart.py
+++ backend/core/management/commands/firststart.py
@@ -11,18 +11,31 @@ logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
+    help = """Command for preparing the application for first start.
+    Loads fixtures and adds a task for scraping markers if necessary.
+    """
+
     def handle(self, *args, **options):
-        """Preparing for first start of application.
-        Loads fixtures and add a task for scraping markers,
-        if there are no any markers in the DB.
-        """
-        if not Tag.objects.all().exists():
+        """Handle command execution."""
+        self.load_tags()
+        self.load_kinds()
+        self.scrape_markers()
+
+    def load_tags(self):
+        """Load tags if they don't exist in the database."""
+        if not Tag.objects.exists():
             call_command("loaddata", "fixtures/tag.json")
-            logger.info("Preparing tags for first start of app complete.")
-        if not Kind.objects.all().exists():
+            logger.info("Tags loaded for first start of app.")
+
+    def load_kinds(self):
+        """Load kinds if they don't exist in the database."""
+        if not Kind.objects.exists():
             call_command("loaddata", "fixtures/kind_group.json")
             call_command("loaddata", "fixtures/kind.json")
-            logger.info("Preparing kinds for first start of app complete.")
-        if not Marker.objects.all().exists():
+            logger.info("Kinds loaded for first start of app.")
+
+    def scrape_markers(self):
+        """Create task for scrape markers if they don't exist in the database."""
+        if not Marker.objects.exists():
             run_scrap_markers_main.delay()
-            logger.info("Task for preparing markers for first start of app complete.")
+            logger.info("Task for scraping markers scheduled for first start of app.")
