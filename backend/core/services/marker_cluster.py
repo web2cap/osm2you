7e0227b39db61b281f3cf20410ff8c59522cf108
Pavel <web2cap@gmail.com>
2024-02-14 17:40:11 +0100

    servises for clustering

---
+++ backend/core/services/marker_cluster.py
@@ -0,0 +1,69 @@
+from django.conf import settings
+from django.db import connection
+
+from core.models.markers import MarkerCluster, UpdatedMarkerCluster
+from core.models.tags import Kind
+
+CLUSTERING = getattr(settings, "CLUSTERING", {})
+
+
+class MarkerClusterService:
+
+    @staticmethod
+    def create_clusters():
+        """Creates clusters for all square_size and store it to temporary UpdatedMarkerCluster."""
+        for square_size in CLUSTERING["square_size"]:
+            marker_clusters = MarkerClusterService._calculate_marker_clusters(
+                square_size
+            )
+            MarkerClusterService._save_marker_clusters(marker_clusters, square_size)
+
+    @staticmethod
+    def copy_clusters_into_main_model():
+        """Copy all data from temporary UpdatedMarkerCluster to production MarkerCluster."""
+        sql_query = """
+            INSERT INTO core_markercluster
+            SELECT * FROM core_updatedmarkercluster;
+        """
+        with connection.cursor() as cursor:
+            cursor.execute(sql_query)
+
+    @staticmethod
+    def clear_marker_clusters():
+        """Clear existing MarkerCluster data."""
+        return MarkerCluster.objects.all().delete()
+
+    @staticmethod
+    def clear_updated_marker_clusters():
+        """Clear existing UpdatedMarkerCluster data."""
+        return UpdatedMarkerCluster.objects.all().delete()
+
+    @staticmethod
+    def _calculate_marker_clusters(square_size):
+        """Calculate markers count for each square with setted size in the map.
+        Calculate averege position of markers for every sqare like a marker cluster position.
+        """
+        sql_query = f"""
+            SELECT ST_Centroid(ST_Collect(location)) as squared_location, COUNT(m.id) as marker_count
+            FROM core_marker as m
+            LEFT JOIN core_markerkind tm ON tm.marker_id = m.id 
+            LEFT JOIN core_kind tk on tk.id = tm.kind_id  
+            WHERE tk.kind_class  = '{Kind.KIND_CLASS_MAIN}'
+            GROUP BY ST_SnapToGrid(location, {square_size});
+        """
+
+        with connection.cursor() as cursor:
+            cursor.execute(sql_query)
+            marker_clusters = cursor.fetchall()
+
+        return marker_clusters
+
+    @staticmethod
+    def _save_marker_clusters(marker_clusters, square_size):
+        """Save markers clusters into temporary UpdatedMarkerCluster."""
+        for marker_cluster in marker_clusters:
+            UpdatedMarkerCluster.objects.create(
+                location=marker_cluster[0],
+                square_size=square_size,
+                markers_count=marker_cluster[1],
+            )
