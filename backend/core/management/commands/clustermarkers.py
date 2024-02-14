719065c671616469f996b13c038a15a6229b9d9f
Pavel <web2cap@gmail.com>
2024-02-14 17:40:39 +0100

    only call manager

--- backend/core/management/commands/clustermarkers.py
+++ backend/core/management/commands/clustermarkers.py
@@ -1,70 +1,16 @@
-import logging
-
-from django.conf import settings
 from django.core.management.base import BaseCommand
-from django.db import connection
 
-from core.models.markers import MarkerCluster, UpdatedMarkerCluster
-from core.models.tags import Kind
-
-CLUSTERING = getattr(settings, "CLUSTERING", {})
-
-
-logger = logging.getLogger(__name__)
+from core.managers.marker_cluster import MarkerClusterManager
 
 
 class Command(BaseCommand):
-    help = "Create MarkerClusters based on Marker locations"
+    help = "Update MarkerClusters based on Marker locations"
 
     def handle(self, *args, **options):
-        self.clear_clusters(UpdatedMarkerCluster)
-        for square_size in CLUSTERING["square_size"]:
-            markers = self.create_marker_clusters(square_size)
-            self.update_marker_clusters(markers, square_size)
-        self.move_clusters_into_main_model()
-        self.stdout.write(self.style.SUCCESS("MarkerClusters created successfully"))
-
-    def clear_clusters(self, model):
-        """Clear existing MarkerCluster data."""
-        return model.objects.all().delete()
-
-    def create_marker_clusters(self, square_size):
-        """Calculate clusters for each square."""
-
-        sql_query = f"""
-            SELECT ST_Centroid(ST_Collect(location)) as squared_location, COUNT(m.id) as marker_count
-            FROM core_marker as m
-            LEFT JOIN core_markerkind tm ON tm.marker_id = m.id 
-            LEFT JOIN core_kind tk on tk.id = tm.kind_id  
-            WHERE tk.kind_class  = '{Kind.KIND_CLASS_MAIN}'
-            GROUP BY ST_SnapToGrid(location, {square_size});
-        """
-
-        with connection.cursor() as cursor:
-            cursor.execute(sql_query)
-            marker_clusters = cursor.fetchall()
-        return marker_clusters
-
-    def update_marker_clusters(self, marker_clusters, square_size):
-        """Save the clusters in the UpdatedMarkerCluster table."""
-
-        for marker_cluster in marker_clusters:
-            UpdatedMarkerCluster.objects.create(
-                location=marker_cluster[0],
-                square_size=square_size,
-                markers_count=marker_cluster[1],
+        status, code = MarkerClusterManager.update_clusters()
+        if status:
+            self.stdout.write(
+                self.style.SUCCESS("Markers clusters updated successfully.")
             )
-
-    def move_clusters_into_main_model(self):
-        """Clear MarkerCluster and copy data from UpdatedMarkerCluster to MarkerCluster."""
-
-        if not UpdatedMarkerCluster.objects.all().count():
-            raise ValueError("UpdatedMarkerCluster should not be empty.")
-
-        self.clear_clusters(MarkerCluster)
-        sql_query = """
-            INSERT INTO core_markercluster
-            SELECT * FROM core_updatedmarkercluster;
-        """
-        with connection.cursor() as cursor:
-            cursor.execute(sql_query)
+        else:
+            self.stdout.write(self.style.ERROR(code))
