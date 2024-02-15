ec95aa486022bfa1759a54afc21a64128e7ec1f2
Pavel <web2cap@gmail.com>
2024-02-15 19:09:01 +0100

    set marker kind, get suitable kind

--- backend/core/services/kinds.py
+++ backend/core/services/kinds.py
@@ -1,4 +1,6 @@
-from core.models.tags import Kind
+from django.db.models import F, OuterRef, Subquery
+
+from core.models.tags import Kind, MarkerKind, TagValue
 
 
 class KindService:
@@ -21,6 +23,43 @@ class KindService:
     def get_related_kinds():
         """Retrieves related kinds."""
         return KindService._get_kinds_by_class(Kind.KIND_CLASS_RELATED)
+
+    @staticmethod
+    def _get_suitable_kind_for_marker(marker):
+        """Choose a most suitable kind by tags."""
+
+        marker_tag_value_subquery = TagValue.objects.filter(
+            value=F("value"),
+            marker=marker,
+            tag_id=OuterRef("tag_id"),
+        ).values("id")
+
+        return (
+            Kind.objects.annotate(
+                matching_tag_value=Subquery(marker_tag_value_subquery)
+            )
+            .order_by("priority")
+            .filter(matching_tag_value__isnull=False)
+            .first()
+        )
+
+    @staticmethod
+    def set_marker_kind(marker):
+        """
+        Sets the kind for a marker if it has not been set already.
+
+        Args:
+            marker (Marker)
+
+        Returns:
+            tuple: MarkerKind instance and a boolean indicating whether a kind assigned to the marker.
+        """
+        marker_kinds = MarkerKind.objects.filter(marker=marker)
+        if marker_kinds.exists():
+            return marker_kinds.first(), False
+        new_kind = KindService._get_suitable_kind_for_marker(marker)
+        if new_kind:
+            return MarkerKind.objects.create(marker=marker, kind=new_kind), True
 
     @staticmethod
     def _get_kinds_by_class(kind_class):
