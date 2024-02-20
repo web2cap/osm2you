a177680df7faa4945412a17f8ce658adddcdd011
Pavel <web2cap@gmail.com>
2024-02-20 21:26:14 +0100

    separate test marker viewset

--- tests/unit/api/viewsets/test_marker.py
+++ tests/unit/api/viewsets/test_marker.py
@@ -104,21 +104,3 @@ class TestMarkerViewset:
         assert (
             marker_with_author_story.stories.first() in queryset.first().stories.all()
         ), "No story in markers queryset"
-
-
-class TestStoryViewset:
-    """Tests marker viewset."""
-
-    @pytest.mark.django_db
-    def test_get_queryset(
-        self, story_viewset, simple_story, user_owner_instance, simple_marker
-    ):
-        """Test are story, marker, author in story queryset."""
-        queryset = story_viewset.get_queryset()
-        assert simple_story in queryset, "No story in story queryset"
-        assert (
-            queryset.first().author == user_owner_instance
-        ), "No story author in story queryset"
-        assert (
-            queryset.first().marker == simple_marker
-        ), "No story marker in story queryset"
