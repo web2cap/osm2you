6c1574bf536c8d32e599e3ac5df13711b42c5b6c
web2cap <web2cap@gmail.com>
2024-03-24 23:59:55 +0100

    use degress instead meters for radius

--- backend/core/services/related_markers.py
+++ backend/core/services/related_markers.py
@@ -1,5 +1,6 @@
 from api.serializers.markers import MarkerRelatedSerializer
 from django.conf import settings
+from django.contrib.gis.measure import D
 from django.db.models import Prefetch
 
 from core.models.markers import Marker
@@ -10,13 +11,16 @@ MARKERS_RELATED_IN_RADIUS = getattr(settings, "MARKERS_RELATED_IN_RADIUS", 5000)
 
 class RelatedMarkers:
     @staticmethod
-    def _get_related_markers_queryset(marker, radius):
+    def _get_related_markers_queryset(marker, radius_meters):
         """
         Get the queryset for related markers within a specified radius from a given marker.
         Each related marker includes additional information about its kind."""
 
+        # Convert meters to degrees for the radius
+        radius_degrees = RelatedMarkers._convert_meters_to_degrees(radius_meters)
+
         return (
-            Marker.objects.filter(location__distance_lte=(marker.location, radius))
+            Marker.objects.filter(location__dwithin=(marker.location, radius_degrees))
             .exclude(id=marker.id)
             .select_related("kind")
             .prefetch_related(
@@ -26,6 +30,12 @@ class RelatedMarkers:
                 )
             )
         )
+
+    @staticmethod
+    def _convert_meters_to_degrees(meters):
+        """Convert miles to degrees."""
+
+        return meters / 40000000 * 360
 
     @staticmethod
     def get_related_markers_data(marker):
