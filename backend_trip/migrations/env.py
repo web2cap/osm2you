ec17403dad9264941d1655e5c9a7a69000079654
web2cap <web2cap@gmail.com>
2024-09-26 10:25:41 +0200

    trip model

--- backend_trip/migrations/env.py
+++ backend_trip/migrations/env.py
@@ -2,7 +2,9 @@ from logging.config import fileConfig
 
 from alembic import context
 from database import DATABASE_URL, Base
+from external.models import Marker, User
 from sqlalchemy import engine_from_config, pool
+from trip.models import Trip
 
 config = context.config
 
@@ -11,8 +13,9 @@ config.set_main_option("sqlalchemy.url", f"{DATABASE_URL}?async_fallback=True")
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-
 target_metadata = Base.metadata
+
+__all_models__ = [Trip, User, Marker]
 
 EXISTING_TABLES_NAMESPACE = (
     "auth_",
@@ -30,22 +33,14 @@ def include_object(object, name, type_, reflected, compare_to):
     return True
 
 
-def run_migrations_offline() -> None:
-    """Run migrations in 'offline' mode.
-
-    This configures the context with just a URL
-    and not an Engine, though an Engine is acceptable
-    here as well.  By skipping the Engine creation
-    we don't even need a DBAPI to be available.
-
-    Calls to context.execute() here emit the given string to the
-    script output.
-
-    """
+def run_migrations_offline():
+    """Run migrations in 'offline' mode."""
     url = config.get_main_option("sqlalchemy.url")
     context.configure(
         url=url,
         target_metadata=target_metadata,
+        # include_schemas=True,
+        include_object=include_object,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
     )
@@ -54,15 +49,10 @@ def run_migrations_offline() -> None:
         context.run_migrations()
 
 
-def run_migrations_online() -> None:
-    """Run migrations in 'online' mode.
-
-    In this scenario we need to create an Engine
-    and associate a connection with the context.
-
-    """
+def run_migrations_online():
+    """Run migrations in 'online' mode."""
     connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
+        config.get_section(config.config_ini_section),
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
@@ -71,6 +61,7 @@ def run_migrations_online() -> None:
         context.configure(
             connection=connection,
             target_metadata=target_metadata,
+            # include_schemas=True,
             include_object=include_object,
         )
 
---
+++ backend_trip/trip/models.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, Date, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+
+from database import Base
+
+
+class Trip(Base):
+    __tablename__ = "trips"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    marker_id = Column(
+        Integer, ForeignKey("public.core_marker.id", ondelete="CASCADE"), nullable=False
+    )
+    user_id = Column(Integer, ForeignKey("public.core_user.id", ondelete="CASCADE"), nullable=False)
+
+    create_date = Column(Date, nullable=False)
+    start_date = Column(Date, nullable=False)
+    end_date = Column(Date, nullable=False)
+    description = Column(String, nullable=False)
+
+    marker = relationship("Marker", back_populates="trips")
+    user = relationship("User", back_populates="trips")
