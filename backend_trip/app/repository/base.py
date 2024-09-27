007742feec9ca02cfa8a3869c4b0487a74cb6d30
web2cap <web2cap@gmail.com>
2024-09-27 18:07:47 +0200

    BaseRepository without create instance

--- backend_trip/app/repository/base.py
+++ backend_trip/app/repository/base.py
@@ -1,46 +1,43 @@
-from typing import Generic, List, Optional, Type, TypeVar
-
 from sqlalchemy import delete, insert, select
-from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.database import async_session_maker
 
-T = TypeVar("T")
 
+class BaseRepository:
+    model = None
 
-class BaseRepository(Generic[T]):
-    model: Type[T] = None
-
-    def __init__(self, session: AsyncSession = None):
-        self.session = session or async_session_maker()
-
-    async def find_by_id(self, model_id: int) -> Optional[T]:
-        async with self.session as session:
+    @classmethod
+    async def find_by_id(self, model_id: int):
+        async with async_session_maker as session:
             query = select(self.model).filter_by(id=model_id)
             result = await session.execute(query)
             return result.scalar_one_or_none()
 
-    async def find_one_or_none(self, **filter_by) -> Optional[T]:
-        async with self.session as session:
+    @classmethod
+    async def find_one_or_none(self, **filter_by):
+        async with async_session_maker as session:
             query = select(self.model).filter_by(**filter_by)
             result = await session.execute(query)
             return result.scalar_one_or_none()
 
-    async def find_all(self, **filter_by) -> List[T]:
-        async with self.session as session:
+    @classmethod
+    async def find_all(self, **filter_by):
+        async with async_session_maker as session:
             query = select(self.model).filter_by(**filter_by)
             result = await session.execute(query)
             return result.scalars().all()
 
-    async def insert_data(self, **data) -> T:
-        async with self.session as session:
+    @classmethod
+    async def insert_data(self, **data):
+        async with async_session_maker as session:
             query = insert(self.model).values(**data).returning(self.model)
             result = await session.execute(query)
             await session.commit()
             return result.scalars().first()
 
-    async def delete_by_id(self, model_id: int) -> Optional[T]:
-        async with self.session as session:
+    @classmethod
+    async def delete_by_id(self, model_id: int):
+        async with async_session_maker as session:
             query = delete(self.model).where(self.model.id == model_id).returning(self.model)
             result = await session.execute(query)
             await session.commit()
