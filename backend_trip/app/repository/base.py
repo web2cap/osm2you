09a1460452be55d6e5ea86971255b2a00780cc35
web2cap <web2cap@gmail.com>
2024-09-27 18:26:44 +0200

    fix async_ses_maker as func

--- backend_trip/app/repository/base.py
+++ backend_trip/app/repository/base.py
@@ -8,28 +8,28 @@ class BaseRepository:
 
     @classmethod
     async def find_by_id(self, model_id: int):
-        async with async_session_maker as session:
+        async with async_session_maker() as session:
             query = select(self.model).filter_by(id=model_id)
             result = await session.execute(query)
             return result.scalar_one_or_none()
 
     @classmethod
     async def find_one_or_none(self, **filter_by):
-        async with async_session_maker as session:
+        async with async_session_maker() as session:
             query = select(self.model).filter_by(**filter_by)
             result = await session.execute(query)
             return result.scalar_one_or_none()
 
     @classmethod
     async def find_all(self, **filter_by):
-        async with async_session_maker as session:
+        async with async_session_maker() as session:
             query = select(self.model).filter_by(**filter_by)
             result = await session.execute(query)
             return result.scalars().all()
 
     @classmethod
     async def insert_data(self, **data):
-        async with async_session_maker as session:
+        async with async_session_maker() as session:
             query = insert(self.model).values(**data).returning(self.model)
             result = await session.execute(query)
             await session.commit()
@@ -37,7 +37,7 @@ class BaseRepository:
 
     @classmethod
     async def delete_by_id(self, model_id: int):
-        async with async_session_maker as session:
+        async with async_session_maker() as session:
             query = delete(self.model).where(self.model.id == model_id).returning(self.model)
             result = await session.execute(query)
             await session.commit()
--- backend_trip/app/repository/trip.py
+++ backend_trip/app/repository/trip.py
@@ -1,6 +1,7 @@
 from sqlalchemy import select
 from sqlalchemy.orm import joinedload
 
+from app.core.database import async_session_maker
 from app.models.trip import Trip
 from app.repository.base import BaseRepository
 
@@ -10,7 +11,7 @@ class TripRepository(BaseRepository):
 
     @classmethod
     async def find_by_id(self, model_id: int):
-        async with self.session as session:
+        async with async_session_maker() as session:
             query = (
                 select(self.model)
                 .options(joinedload(Trip.marker), joinedload(Trip.user))
