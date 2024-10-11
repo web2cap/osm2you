532918aeb1364c60de2227a12e465a93e0c9a188
web2cap <web2cap@gmail.com>
2024-10-11 22:03:46 +0200

    make alembic migrations

--- tests/trip_backend/fixtures/fixture_database.py
+++ tests/trip_backend/fixtures/fixture_database.py
@@ -1,7 +1,11 @@
+import subprocess
+
 import pytest
 import sqlalchemy as sa
 from app.core.config import settings
 from app.core.database import async_session_maker
+
+SQL_DUMP_PATH = "../tests/trip_backend/fixtures/fixture_data_test_db.sql"
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -9,24 +13,24 @@ async def prepare_database(request):
     assert settings.MODE == "TEST"
 
     reuse_db = request.config.getoption("--reuse-db", False)
-    sql_dump_file = "../tests/trip_backend/fixtures/fixture_data_test_db.sql"
-
     if not reuse_db:
         print("Deleting and recreating the database.")
-        drop_all_tables_sql = """
-        DO $$ DECLARE
-            r RECORD;
-        BEGIN
-            -- Loop through all table names in the current schema, excluding 'spatial_' prefixed tables
-            FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = 'public' AND tablename NOT LIKE 'spatial_%') LOOP
-                EXECUTE 'DROP TABLE IF EXISTS ' || quote_ident(r.tablename) || ' CASCADE';
-            END LOOP;
-        END $$;
-        """
-
         async with async_session_maker() as session:
+            # delete all existings tables
+            drop_all_tables_sql = """
+            DO $$ DECLARE
+                r RECORD;
+            BEGIN
+                -- Loop through all table names in the current schema, excluding 'spatial_' prefixed tables
+                FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = 'public' AND tablename NOT LIKE 'spatial_%') LOOP
+                    EXECUTE 'DROP TABLE IF EXISTS ' || quote_ident(r.tablename) || ' CASCADE';
+                END LOOP;
+            END $$;
+            """
             await session.execute(sa.text(drop_all_tables_sql))
-            with open(sql_dump_file, "r") as file:
+
+            # load django dump
+            with open(SQL_DUMP_PATH, "r") as file:
                 sql_statements = file.read().split(";")
                 for statement in sql_statements:
                     clean_statement = statement.strip()
@@ -37,6 +41,9 @@ async def prepare_database(request):
                             print(f"Error executing statement: {clean_statement}")
                             raise e
             await session.commit()
+
+        # make fast-api migrations
+        subprocess.run(["alembic", "upgrade", "head"], check=False)
     yield
 
 
