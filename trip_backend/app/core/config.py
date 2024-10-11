77b0f0689db92987e4d618795d6941ca5c2cd4fa
web2cap <web2cap@gmail.com>
2024-10-11 18:19:46 +0200

    current or test database in conf

--- trip_backend/app/core/config.py
+++ trip_backend/app/core/config.py
@@ -6,11 +6,13 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     MODE: Literal["DEV", "TEST", "PROD"]
+
     DB_HOST: str
     DB_PORT: int
     DB_NAME: str
     DB_USER: str
     DB_PASSWORD: str
+    DB_TEST_NAME: str
 
     ST_SECRET_KEY: str
 
