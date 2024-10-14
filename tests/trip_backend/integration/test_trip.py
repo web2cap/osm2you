b03547f70439c7b2b72e342a5dd7278e688ab38e
web2cap <web2cap@gmail.com>
2024-10-14 12:04:55 +0200

    add test trip get by id

--- tests/trip_backend/integration/test_trip.py
+++ tests/trip_backend/integration/test_trip.py
@@ -3,25 +3,30 @@ from app.models.trip import Trip
 from httpx import AsyncClient
 
 
-@pytest.mark.asyncio
-async def test_get_trip_by_id(ac: AsyncClient, create_simple_trip: Trip):
-    trip = create_simple_trip
-    response = await ac.get(f"/v1/trip/{trip.id}")
+class TestTrip:
+    URL_TRIP = "/v1/trip/"
 
-    assert response.status_code == 200
-    data = response.json()
+    @pytest.mark.asyncio
+    async def test_get_trip_by_id(self, ac: AsyncClient, create_simple_trip: Trip):
+        trip = create_simple_trip
+        response = await ac.get(f"{self.URL_TRIP}{trip.id}")
 
-    assert data["id"] == trip.id
-    assert data["description"] == trip.description
-    assert data["create_date"] == str(trip.create_date)
-    assert data["start_date"] == str(trip.start_date)
-    assert data["end_date"] == str(trip.end_date)
+        assert response.status_code == 200
+        data = response.json()
 
-    assert data["user"]["id"] == 2
-    assert "username" in data["user"] and len(data["user"]["username"])
-    assert "first_name" in data["user"]
-    assert "last_name" in data["user"]
+        assert data["id"] == trip.id
+        assert data["description"] == trip.description
+        assert data["create_date"] == str(trip.create_date)
+        assert data["start_date"] == str(trip.start_date)
+        assert data["end_date"] == str(trip.end_date)
 
-    assert data["marker"]["id"] == trip.marker_id
-    assert "name" in data["marker"]
-    assert "location" in data["marker"] and isinstance(data["marker"]["location"], list)
+        assert data["user"]["id"] == trip.user_id
+        assert "username" in data["user"] and len(data["user"]["username"])
+        assert "first_name" in data["user"]
+        assert "last_name" in data["user"]
+
+        assert data["marker"]["id"] == trip.marker_id
+        assert "name" in data["marker"]
+        assert "location" in data["marker"] and isinstance(
+            data["marker"]["location"], list
+        )
