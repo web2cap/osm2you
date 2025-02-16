1f22cbbc18cd8af34171050fec3e605912ede423
web2cap <web2cap@gmail.com>
2025-02-16 21:51:04 +0100

    Fix auth token in headers

--- trip_backend/app/core/dependencies.py
+++ trip_backend/app/core/dependencies.py
@@ -1,11 +1,15 @@
+from fastapi import Security
+from fastapi.security import OAuth2PasswordBearer
+
+
 from app.core.exceptions import IncativeUserException, UserNotFoundException
 from app.repository.user import UserRepository
 from app.utils.id_from_token import get_user_id_from_token
 
-ALGORITHM = "HS256"
+OAUTH2_SHEME =  OAuth2PasswordBearer(tokenUrl="token")
 
-
-async def get_current_user(token: str):
+async def get_current_user(token: str = Security(OAUTH2_SHEME)):
+    """Extracts the user from the Bearer token in Authorization header."""
     user_id = get_user_id_from_token(token)
     user = await UserRepository.find_by_id(user_id)
     if not user:
