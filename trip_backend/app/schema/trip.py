9c6a28c9a34255b0c2f1e027f3d465825e7c0504
web2cap <web2cap@gmail.com>
2025-02-24 22:39:51 +0100

    Fix trip dates validation

--- trip_backend/app/schema/trip.py
+++ trip_backend/app/schema/trip.py
@@ -1,11 +1,12 @@
-from datetime import date
+from datetime import date, datetime
 
-from pydantic import BaseModel, field_validator
+from pydantic import BaseModel, model_validator
 
 from app.core.exceptions import InvalidEndDateException, InvalidStartDateException
 from app.schema.marker import SMarker
 from app.schema.user import SUser
 
+DATE_FORMAT = "%Y-%m-%d"
 
 class STripDetailed(BaseModel):
     id: int
@@ -24,20 +25,32 @@ class STripValidateDates(BaseModel):
     start_date: date
     end_date: date
 
-    @field_validator("end_date")
+    @model_validator(mode="before")
     @classmethod
-    def validate_end_date(cls, v, info):
-        if v < date.today():
-            raise InvalidEndDateException()
-        return v
+    def validate_dates(cls, values):
 
-    @field_validator("start_date")
-    @classmethod
-    def validate_start_date(cls, v, info):
-        end_date = info.data.get("end_date")
-        if end_date and v > end_date:
+        start_date = values.get("start_date")
+        end_date = values.get("end_date")
+
+        # Let Pydantic handle missing fields
+        if not end_date or not start_date:
+            return values  
+
+        if isinstance(start_date, str):
+            start_date = datetime.strptime(start_date, DATE_FORMAT).date()
+        if isinstance(end_date, str):
+            end_date = datetime.strptime(end_date, DATE_FORMAT).date()
+
+        # can't finished before today
+        if end_date < date.today():
+            raise InvalidEndDateException()
+
+        # can't ends before start
+        if end_date < start_date:
             raise InvalidStartDateException()
-        return v
+
+        return values
+
 
 
 class STripCreate(STripValidateDates):
