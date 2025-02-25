5b1769e977503f40031e45b3622c479459cc83db
web2cap <web2cap@gmail.com>
2025-02-25 01:07:53 +0100

    Test add fixture user not owner

--- tests/trip_backend/fixtures/fixture_user.py
+++ tests/trip_backend/fixtures/fixture_user.py
@@ -8,3 +8,10 @@ def current_user():
     return User(
         id=2, username="testuser", first_name="Test", last_name="User", is_active=True
     )
+
+
+@pytest.fixture
+def non_owner_user():
+    return User(
+        id=1, username="otheruser", first_name="Other", last_name="User", is_active=True
+    )
